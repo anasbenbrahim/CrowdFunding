277d008d51e0731fdcc5b7673ca318ff6f357c26
anasbenbrahim <anasbenbrahim491@gmail.com>
2025-06-28 15:52:40 +0100

    Update repositories

--- src/main/java/me/project/crowdfunding/Repository/CommunityRepository.java
+++ src/main/java/me/project/crowdfunding/Repository/CommunityRepository.java
@@ -2,6 +2,8 @@ package me.project.crowdfunding.Repository;
 
 import me.project.crowdfunding.Entity.Community;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommunityRepository extends JpaRepository<Community, Long> {
 }
